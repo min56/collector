6082cd1b29f4465b68365b3525e25e2191a6f38d
min56 <minchaeg85@gmail.com>
2023-01-10 14:08:56 +0900

    [ ADD ] 코틀린으로 변경

--- src/main/java/com/example/collecter/domain/user/domain/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.collecter.domain.user.domain.repository;
-
-import com.example.collecter.domain.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<UUID, User> {
-}
---
+++ src/test/kotlin/com/example/collecter/CollecterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.collecter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CollecterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
